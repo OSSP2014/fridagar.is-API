0a88078fe9bb6fb198228aef011abfa62aa485ee
Bjarki Sorens <bjerkins8@gmail.com>
2014-05-31 19:26:28 +0000

    adding parser

---
+++ parse_ical.py
@@ -0,0 +1,55 @@
+from icalendar import Calendar, Event
+import urllib2
+
+'''
+	Parses .ics files retrived from www.kayaposoft.com
+'''
+class IcsParser:
+
+	'''Constructor for IcsParser'''
+	def __init__(self):
+		self.icsFile = None
+		self.url = 'http://www.kayaposoft.com/enrico/ics/v1.0?country=%s&fromDate=01-01-%s&toDate=31-12-%s'
+
+	'''
+		Method getIcs
+		Parameters:
+			country: string, e.g. 'isl', 'eng', 'usa'
+			fromYear: string from 01-01, e.g. '2014', '2015'
+			toYear: string to 31-12, e.g. '2015', '2016'
+		Returns: dictionary {'holidayName': vText('example'), 'holidayDate': datetime.date(2014, 12, 25)}
+	'''
+	def getIcs(self, country, fromYear, toYear):
+		self.__downloadFile__(country, fromYear, toYear)
+		parsed = self.__parseToDictionary__()
+		return parsed
+
+	'''
+		Private method __downloadFile__
+		self.icsFile is the downloaded file
+	'''
+	def __downloadFile__(self, country, fromYear, toYear):
+		response = urllib2.urlopen(self.url % (country, fromYear, toYear))
+		self.icsFile = response.read()
+		response.close()
+
+	'''
+		Private method __parseToDictionary__
+		parses .ics to Dictionary
+		Returns: dictionary {'holidayName': vText('example'), 'holidayDate': datetime.date(2014, 12, 25)}
+	'''
+	def __parseToDictionary__(self):
+		if self.icsFile is not None:
+			result = []
+			gcal = Calendar.from_ical(self.icsFile)
+			for component in gcal.walk():
+				if component.name == "VEVENT":
+					print a
+					result.append({'holidayName': component.get('summary'), 'holidayDate': component.get('dtstart').dt})
+		
+		return result
+
+
+if __name__ == '__main__':
+	parser = IcsParser()
+	print parser.getIcs('isl', '2014', '2014')
