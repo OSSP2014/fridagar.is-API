42cfbb1c73b42dac9fdf77ac3d34778cd88ed9d5
Tindur <tindur.h@gmail.com>
2014-06-11 14:07:52 +0000

    flask cors module woking?

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask.ext.cors import cross_origin
 from parse_ical import IcsParser
 from parse_html import HtmlParser
 from cache_function import memorize
@@ -11,14 +12,17 @@ baseUrl = "/api"
 app = Flask(__name__)
 
 @app.route("/")
+# @cross_origin()
 def hello():
 	return "Hello World!"
 
 @app.route(baseUrl + "/holidays/<language>/<fromYear>/<toYear>")
+@cross_origin()
 def holidays(language, fromYear, toYear):
 	return jsonify(result=getHolidays(language, fromYear, toYear))
 
 @app.route(baseUrl + "/countries")
+@cross_origin()
 def countries():
 	return jsonify(result=getCountries())
 
